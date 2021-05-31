d9ef72d26fe9bb40a181bec9b30d13587b9632a7
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-05-31 10:46:34 +0200

    Simple Light DOM example

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createElement } from "lwc";
+import { createElement, setFeatureFlagForTest } from "lwc";
 import App from "x/app";
+
+setFeatureFlagForTest('ENABLE_LIGHT_DOM_COMPONENTS', true);
 
 const elm = createElement("x-app", { is: App });
 document.body.appendChild(elm);
+
+if (elm.firstElementChild.tagName === 'H1') {
+    console.log('x-app is rendered in the light DOM')
+} else {
+    console.log('x-app is rendered in the shadow DOM')
+}
--- src/modules/x/app/app.js
+++ src/modules/x/app/app.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    static shadow = false;
+}
